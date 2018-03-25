1bdc913cf09961bba651674839ac299940512d83
lizzardqueen <ezhi.ne@gmail.com>
2018-03-25 17:06:19 +0300

    свап первого и последнего элемента в отсортированном массиве

--- lesson02/main.py
+++ lesson02/main.py
@@ -1,5 +1,6 @@
 import random
 
+#функция, которая создает массив из 10ти случайных чисел
 def creatrandmas():
     m = []
     for i in range(10):
@@ -8,5 +9,10 @@ def creatrandmas():
 
 sortedmasv = creatrandmas()
 print (sortedmasv)
+#сортировка массива
 sortedmasv = sorted(sortedmasv)
 print (sortedmasv)
+
+sortedmasv[0],sortedmasv[9] = sortedmasv[9],sortedmasv[0]
+print (sortedmasv)
+
