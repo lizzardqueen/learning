6089abb4bf73c6388f788e1b5fa29f152f824f75
lizzardqueen <ezhi.ne@gmail.com>
2018-03-25 15:19:19 +0300

    сложение чисел из консоли

--- lesson01/main.py
+++ lesson01/main.py
@@ -1,3 +1,5 @@
-text = ""
-text = input("Введи текст ")
-print ('Мой текст:' + text)
+x = float(input('1е число: '))
+y = float(input('2е число: '))
+z = float(input('3е число: '))
+s = x + y + z
+print('Сумма 3х чисел: ' + str(s))
