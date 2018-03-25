7c2744c71565bdd6b57cd656ed0910de88deba49
lizzardqueen <ezhi.ne@gmail.com>
2018-03-25 16:51:14 +0300

    сортировка уже существующего массива

--- lesson02/main.py
+++ lesson02/main.py
@@ -1,8 +1,12 @@
 import random
+
 def creatrandmas():
     m = []
-    for i in range(9):
+    for i in range(10):
         m.append(random.randint(0, 999))
     return m
-masv = creatrandmas()
-print (masv)
+
+sortedmasv = creatrandmas()
+print (sortedmasv)
+sortedmasv = sorted(sortedmasv)
+print (sortedmasv)
