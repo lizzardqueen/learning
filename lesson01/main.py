2446a7aa30f45b21ca49aa4b390b8187cef07738
lizzardqueen <ezhi.ne@gmail.com>
2018-03-25 15:41:10 +0300

    сложение чисел из консоли с проверкой ввода

--- lesson01/main.py
+++ lesson01/main.py
@@ -1,5 +1,20 @@
-x = float(input('1е число: '))
-y = float(input('2е число: '))
-z = float(input('3е число: '))
+x = input('1е число: ')
+y = input('2е число: ')
+z = input('3е число: ')
+try:
+    x = float(x)
+except:
+    print ('Написано же число, а ты что вводишь?')
+    quit()
+try:
+    y = float(y)
+except:
+    print ('Написано же число, а ты что вводишь?')
+    quit()
+try:
+    z = float(z)
+except:
+    print ('Написано же число, а ты что вводишь?')
+    quit()
 s = x + y + z
 print('Сумма 3х чисел: ' + str(s))
